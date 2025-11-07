939ea14eef80717b84f0cacaeb98418c442ebd2b
cioldnag <loic.gand@gmail.com>
2025-11-07 23:15:32 +0100

    Keep only stock_updatet.py, .gitignore and README.md

--- stock_update.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-set root=c:\banking_2
-call %root%\Scripts\activate.bat
-cd c:\users\loicg\OneDrive\Documents\IT\banking
-call python stock_update.py
--- tl_2.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-set root=c:\banking_2
-call %root%\Scripts\activate.bat
-cd c:\users\loicg\OneDrive\Documents\IT\banking
-call streamlit run app_bank.py
